3d25e38e068baca16cfae2848fe965a72600a58c
Alex Klaus <alex.a.klaus@gmail.com>
2022-09-07 18:32:10 +1000

    Extended logging for dev environment

--- OpenIdDict.Client.Api/Program.cs
+++ OpenIdDict.Client.Api/Program.cs
@@ -1,5 +1,6 @@
 using AK.OAuthSamples.OpenIdDict.Client.Api.Configuration;
 using AK.OAuthSamples.OpenIdDict.Client.Api.Routes;
+using Microsoft.IdentityModel.Logging;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,11 @@ builder.Services.AddAndConfigureSwagger(settings)
 				.AddAndConfigureAuthorisation(settings.OAuth);
 
 var app = builder.Build();
+
+if (builder.Environment.IsDevelopment() || builder.Environment.IsEnvironment("Local"))
+	// Enable showing extra debug information in the console 
+	IdentityModelEventSource.ShowPII = true; 
+
 app .UseDeveloperExceptionPage()
 	.UseHttpsRedirection()
 	.ConfigureSwagger(settings)
